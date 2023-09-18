4b734b1edd386ccc7df2656481c549340d38890d
Emre Akguel <emre.akguel.1@gmail.com>
2023-09-18 12:36:42 +0200

    extend register page

--- src/pages/public/RegisterScreen.tsx
+++ src/pages/public/RegisterScreen.tsx
@@ -1,18 +1,70 @@
 import React, {JSX} from 'react';
-import {View, Text, Button} from 'react-native';
+import {Text, Button, SafeAreaView, ScrollView, TextInput, StyleSheet, View} from 'react-native';
 
 type Props = {
   navigation: any;
 };
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Name ist erforderlich'),
+  email: Yup.string().email('Ungültige E-Mail').required('E-Mail ist erforderlich'),
+  // ... Weitere Validierungen hier ...
+});
 
 function RegisterScreen({navigation}: Props): JSX.Element {
-  return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <Text>Registrierungsseite</Text>
-    <Button
-      title="Zur aktuellen Registrierung"
-      onPress={() => navigation.navigate('CurrentRegistration')}
-    />
-  </View>;
+  return <SafeAreaView>
+    <ScrollView>
+      <Formik
+        initialValues={{ name: '', email: '', /* ... */ }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+          <View style={styles.container}>
+            <TextInput
+              placeholder={'Name'}
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              value={values.name}
+              style={styles.input}
+            />
+            {errors.name && <Text>{errors.name}</Text>}
+
+            <TextInput
+              placeholder={'E-Mail'}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              style={styles.input}
+            />
+            {errors.email && <Text>{errors.email}</Text>}
+
+            <Button
+              onPress={() => handleSubmit()}
+              title="Abschicken" />
+          </View>
+        )}
+      </Formik>
+    </ScrollView>
+  </SafeAreaView>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 8,
+  },
+  input: {
+    height: 40,
+    margin: 8,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 8,
+  },
+});
 
 export default RegisterScreen;
